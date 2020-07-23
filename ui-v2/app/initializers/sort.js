cfe6ba5c5fed4890e8b3108c3dd3718b63c879d5
kenia <keniavalladarez@gmail.com>
2020-07-23 10:42:45 -0400

    Add sorting to ACLs roles with comparator

--- ui-v2/app/initializers/sort.js
+++ ui-v2/app/initializers/sort.js
@@ -1,5 +1,6 @@
 import service from 'consul-ui/sort/comparators/service';
 import check from 'consul-ui/sort/comparators/check';
+import role from 'consul-ui/sort/comparators/role';
 
 export function initialize(container) {
   // Service-less injection using private properties at a per-project level
@@ -7,6 +8,7 @@ export function initialize(container) {
   const comparators = {
     service: service(),
     check: check(),
+    role: role(),
   };
   Sort.reopen({
     comparator: function(type) {
